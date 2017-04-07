23f78c5bbdd1cb72d665b58ea1c8da45f9b59cfa
canim44 <connorcanim@gmail.com>
2017-04-07 13:50:25 -0400

    Added Library call backbone

--- simpled.cpp
+++ simpled.cpp
@@ -1,3 +1,19 @@
+int simpleSet(char *MachineName, int TCPport, int SecretKey, char *variableName, char *value, int dataLength) {
+	return 0;
+}
+
+int simpleGet(char *MachineName, int TCPport, int SecretKey, char *variableName, char *value, int *resultLength) {
+	return 0;
+}
+
+int simpleDigest(char *MachineName, int TCPport, int SecretKey, char *data, int dataLength, char *result, int *resultLength) {
+	return 0;
+}
+
+int simpleRun(char *MachineName, int TCPport, int SecretKey, char *request, char *result, int *resultLength) {
+	return 0;
+}
+
 int main(){
 	return 0;
 }
