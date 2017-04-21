baf004d41592d34e69ecfaf72cac12963a301bcf
canim44 <connorcanim@gmail.com>
2017-04-20 22:30:36 -0400

    Added ssDigest ssRun (client)

--- ssDigest.c
+++ ssDigest.c
@@ -1,8 +1,10 @@
 #include "csapp.h"
 
 int main(int argc, char* argv[]){
-	int toserverfd, port, secretkey;
-	char* host, value;
+	int toserverfd, port, varSize, type = 0; 
+	unsigned int secretkey;
+	char *host, *value;
+	char *junk = "cat";
 	rio_t rio;
 	if (argc != 5) {
 		fprintf(stderr, "usage: %s <host> <port> <secretKey> <value>\n",
@@ -12,12 +14,23 @@ int main(int argc, char* argv[]){
 
 	host = argv[1];
 	port = atoi(argv[2]);
-	secretkey = atoi(argv[3]);
+	secretkey = htonl(atoi(argv[3]));
 	value = argv[4];
+	varSize = sizeof(value);
 
 	toserverfd = open_clientfd(host, port);
 	Rio_readinitb(&rio, toserverfd);
-	
+	// Send client secretkey to server
+	Rio_writen(toserverfd, &secretkey, sizeof(int));
+	// Send type of process (DIGEST, 2) to server
+	Rio_writen(toserverfd, &type, 1);
+	// Send three bytes of padding to server
+	Rio_writen(toserverfd, &junk, 3);
+	// Send length of value
+	Rio_writen(toserverfd, &varSize, 4);
+	// Send value to server
+	Rio_writen(toserverfd, &value, varSize);
+
 	Close(toserverfd);
 	exit(0);
 	return 0;
--- ssRun.c
+++ ssRun.c
@@ -1,10 +1,10 @@
 #include "csapp.h"
 
 int main(int argc, char** argv){
-	int toserverfd, port;
+	int toserverfd, port, type = 4;
 	unsigned int secretkey;
-	char* host, *program;
-	char buf[MAXLINE];
+	char *host, *program;
+	char *junk = "cat";
 	rio_t rio;
 	if (argc != 5) {
 		fprintf(stderr, "usage: %s <host> <port> <secretKey> <program>\n",
@@ -19,15 +19,14 @@ int main(int argc, char** argv){
 
 	toserverfd = open_clientfd(host, port);
 	Rio_readinitb(&rio, toserverfd);
-
+	// Send client secretkey to server
 	Rio_writen(toserverfd, &secretkey, sizeof(int));
-	Rio_readnb(&rio, buf, 1);
-
-	if (buf[0] == (char) 1) {
-		fprintf(stderr, "failed\n");
-		Close(toserverfd);
-		return -2;
-	}
+	// Send type of process (RUN, 3) to server
+	Rio_writen(toserverfd, &type, 1);
+	// Send three bytes of padding to server
+	Rio_writen(toserverfd, &junk, 3);
+	// Send program to server
+	Rio_writen(toserverfd, &program, 9);
 
 	Close(toserverfd);
 	return 0;
